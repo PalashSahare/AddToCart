1149a292fa983eef87315b0861e3edb468f69d6d
Palash <43274724+PalashSahare@users.noreply.github.com>
2024-02-10 17:28:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function show_size($event){
+
+    console.log("clicked");
+
+    let id = new String($event.target.id).split("_")[1]; 
+
+
+    document.getElementById("radio-button_"+id).checked = true; 
+
+    setAllDiselect(); 
+
+    if(id==2){
+        document.getElementById("float-menu").classList.add("additional--helper") 
+    }else{
+        document.getElementById("float-menu").classList.remove("additional--helper") 
+    }
+
+    show_table(("size_table_" + id));
+    highlightContainer(("container-box_"+id));
+    document.getElementById("amount").innerHTML = document.getElementById(("price_"+id)).innerHTML;
+    
+}
+
+function highlightContainer(id){
+    let element = document.getElementById(id);
+    element.classList.add("selected_container");
+}
+
+function show_table(id){
+    let element = document.getElementById(id);
+    element.classList.remove("hide");
+}
+
+function setAllDiselect(){
+    let ele = document.getElementsByName("size_table");
+    ele.forEach((element)=>{
+        element.classList.add("hide"); 
+    }); 
+    
+    ele = document.getElementsByName("container-box");
+    ele.forEach((element)=>{
+        element.classList.remove("selected_container"); 
+    });
+}
